88966536d61d80818f1073350ce7e75d07fca280
sukgae <165404505+sukgae@users.noreply.github.com>
2026-06-04 00:28:45 +0900

    디버깅관련 업데이트

---
+++ Project/Debug/Core/Src/subdir.mk
@@ -0,0 +1,66 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (13.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/Src/Comm_Power_Select.c \
+../Core/Src/Comm_select.c \
+../Core/Src/app_freertos.c \
+../Core/Src/bt_comm.c \
+../Core/Src/can_comm.c \
+../Core/Src/main.c \
+../Core/Src/mpu6050.c \
+../Core/Src/readSensor.c \
+../Core/Src/stm32g4xx_hal_msp.c \
+../Core/Src/stm32g4xx_hal_timebase_tim.c \
+../Core/Src/stm32g4xx_it.c \
+../Core/Src/syscalls.c \
+../Core/Src/sysmem.c \
+../Core/Src/system_stm32g4xx.c 
+
+OBJS += \
+./Core/Src/Comm_Power_Select.o \
+./Core/Src/Comm_select.o \
+./Core/Src/app_freertos.o \
+./Core/Src/bt_comm.o \
+./Core/Src/can_comm.o \
+./Core/Src/main.o \
+./Core/Src/mpu6050.o \
+./Core/Src/readSensor.o \
+./Core/Src/stm32g4xx_hal_msp.o \
+./Core/Src/stm32g4xx_hal_timebase_tim.o \
+./Core/Src/stm32g4xx_it.o \
+./Core/Src/syscalls.o \
+./Core/Src/sysmem.o \
+./Core/Src/system_stm32g4xx.o 
+
+C_DEPS += \
+./Core/Src/Comm_Power_Select.d \
+./Core/Src/Comm_select.d \
+./Core/Src/app_freertos.d \
+./Core/Src/bt_comm.d \
+./Core/Src/can_comm.d \
+./Core/Src/main.d \
+./Core/Src/mpu6050.d \
+./Core/Src/readSensor.d \
+./Core/Src/stm32g4xx_hal_msp.d \
+./Core/Src/stm32g4xx_hal_timebase_tim.d \
+./Core/Src/stm32g4xx_it.d \
+./Core/Src/syscalls.d \
+./Core/Src/sysmem.d \
+./Core/Src/system_stm32g4xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/Src/%.o Core/Src/%.su Core/Src/%.cyclo: ../Core/Src/%.c Core/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_NUCLEO_32 -DUSE_HAL_DRIVER -DSTM32G431xx -c -I../Core/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc -I../Drivers/STM32G4xx_HAL_Driver/Inc/Legacy -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/BSP/STM32G4xx_Nucleo -I../Drivers/CMSIS/Device/ST/STM32G4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core-2f-Src
+
+clean-Core-2f-Src:
+	-$(RM) ./Core/Src/Comm_Power_Select.cyclo ./Core/Src/Comm_Power_Select.d ./Core/Src/Comm_Power_Select.o ./Core/Src/Comm_Power_Select.su ./Core/Src/Comm_select.cyclo ./Core/Src/Comm_select.d ./Core/Src/Comm_select.o ./Core/Src/Comm_select.su ./Core/Src/app_freertos.cyclo ./Core/Src/app_freertos.d ./Core/Src/app_freertos.o ./Core/Src/app_freertos.su ./Core/Src/bt_comm.cyclo ./Core/Src/bt_comm.d ./Core/Src/bt_comm.o ./Core/Src/bt_comm.su ./Core/Src/can_comm.cyclo ./Core/Src/can_comm.d ./Core/Src/can_comm.o ./Core/Src/can_comm.su ./Core/Src/main.cyclo ./Core/Src/main.d ./Core/Src/main.o ./Core/Src/main.su ./Core/Src/mpu6050.cyclo ./Core/Src/mpu6050.d ./Core/Src/mpu6050.o ./Core/Src/mpu6050.su ./Core/Src/readSensor.cyclo ./Core/Src/readSensor.d ./Core/Src/readSensor.o ./Core/Src/readSensor.su ./Core/Src/stm32g4xx_hal_msp.cyclo ./Core/Src/stm32g4xx_hal_msp.d ./Core/Src/stm32g4xx_hal_msp.o ./Core/Src/stm32g4xx_hal_msp.su ./Core/Src/stm32g4xx_hal_timebase_tim.cyclo ./Core/Src/stm32g4xx_hal_timebase_tim.d ./Core/Src/stm32g4xx_hal_timebase_tim.o ./Core/Src/stm32g4xx_hal_timebase_tim.su ./Core/Src/stm32g4xx_it.cyclo ./Core/Src/stm32g4xx_it.d ./Core/Src/stm32g4xx_it.o ./Core/Src/stm32g4xx_it.su ./Core/Src/syscalls.cyclo ./Core/Src/syscalls.d ./Core/Src/syscalls.o ./Core/Src/syscalls.su ./Core/Src/sysmem.cyclo ./Core/Src/sysmem.d ./Core/Src/sysmem.o ./Core/Src/sysmem.su ./Core/Src/system_stm32g4xx.cyclo ./Core/Src/system_stm32g4xx.d ./Core/Src/system_stm32g4xx.o ./Core/Src/system_stm32g4xx.su
+
+.PHONY: clean-Core-2f-Src
+
